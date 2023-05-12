7b9e0c66b3f5b1c086d2bab796ce4435232bf1e2
jiangdada1221 <jiangdada12344321@163.com>
2023-05-12 13:14:23 +0000

    fix pip

--- setup.py
+++ setup.py
@@ -1,19 +1,14 @@
 from setuptools import setup, find_packages
 data_files_to_include = [('', ['README.md', 'LICENSE'])]
 
-with open('requirements.txt', 'r') as reqs:
-    requirements = reqs.read().split()
-
-setup(name='tcr2vec',
-      packages=find_packages(),
-      version='1.0.0',
+setup(name='tcr2vec',      
+      version='1.0.1',
       description='TCR2vec is a deep representation learning framework of T-cell receptor sequence and function',
       long_description='Not applicable',
       url='https://github.com/jiangdada1221/TCR2vec',
       author='Yuepeng Jiang',
       author_email='jiangdada12344321@gmail.com',
-      license='GPLv3',
-      install_requires=requirements,
+      license='GPLv3',      
       classifiers=[
             'Development Status :: 3 - Alpha',
             'Intended Audience :: Developers',
@@ -26,6 +21,8 @@ setup(name='tcr2vec',
             'Natural Language :: English',
             'Programming Language :: Python :: 3',
             ],
+      packages = find_packages(),
+      install_requires=['numpy','torch','matplotlib','tqdm','pandas','scikit-learn','Scipy','tape_proteins','biopython'],
       data_files = data_files_to_include,
       include_package_data=True,
       zip_safe=False)
